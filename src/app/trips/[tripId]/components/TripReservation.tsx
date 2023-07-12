30836428ef6b79fe1d35b37a9d913a071e0802ab
VitorLima1998 <vitor.xlr8@gmail.com>
2023-07-12 15:23:54 -0400

    feat: add interface to trip confirmation

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -3,6 +3,7 @@ import Button from '@/components/Button';
 import DatePicker from '@/components/DatePicker';
 import Input from '@/components/Input';
 import { differenceInDays } from 'date-fns';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 import { Controller, useForm } from 'react-hook-form';
 
@@ -36,6 +37,8 @@ const TripReservation = ({
     setError,
   } = useForm<tripReservationForm>();
 
+  const router = useRouter();
+
   const onSubmit = async (data: tripReservationForm) => {
     const response = await fetch('http://localhost:3000/api/trips/check', {
       method: 'POST',
@@ -65,7 +68,7 @@ const TripReservation = ({
     }
 
     if (res?.error?.code === 'INVALID_START_DATE') {
-      setError('startDate', {
+      return setError('startDate', {
         type: 'manual',
         message: 'Data inválida.',
       });
@@ -77,6 +80,12 @@ const TripReservation = ({
         message: 'Data inválida.',
       });
     }
+
+    router.push(
+      `/trips/${tripId}/confirmation?startDate=${data.startDate?.toISOString()}&endDate=${data.endDate?.toISOString()}&guests=${
+        data.guests
+      }`
+    );
   };
 
   const startDate = watch('startDate');
