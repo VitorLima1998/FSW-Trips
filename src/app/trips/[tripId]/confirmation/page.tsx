da2a8afe29242f1ac342c4bdda80f1bc9e9e7e1a
VitorLima1998 <vitor.xlr8@gmail.com>
2023-07-13 22:51:33 -0400

    feat: ensure user is redirected on error in trip confirmation

--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -10,6 +10,7 @@ import { ptBR } from 'date-fns/locale';
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
+import { toast } from 'react-toastify';
 
 const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
   const [trip, setTrip] = useState<Trip | null>();
@@ -49,6 +50,24 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
   }, [status, searchParams, params, router]);
 
   if (!trip) return null;
+
+  const handleBuyClick = async () => {
+    await fetch('http://localhost:3000/api/trips/reservation', {
+      method: 'POST',
+      body: Buffer.from(
+        JSON.stringify({
+          tripId: params.tripId,
+          startDate: searchParams.get('startDate'),
+          endDate: searchParams.get('endDate'),
+          guests: searchParams.get('guests'),
+        })
+      ),
+    });
+
+    toast.success('Reserva realizada com sucesso', {
+      position: 'bottom-center',
+    });
+  };
 
   const startDate = new Date(searchParams.get('startDate') as string);
   const endDate = new Date(searchParams.get('endDate') as string);
@@ -104,7 +123,7 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
         <h3 className='font-semibold mt-5'>Hóspedes</h3>
         <p>{guests} hóspedes</p>
 
-        <Button variant='primary' className='mt-5'>
+        <Button onClick={handleBuyClick} variant='primary' className='mt-5'>
           Finalizar Compra
         </Button>
       </div>
