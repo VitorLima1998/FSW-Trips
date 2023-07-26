2a850cd846cc9e3401eb08ab18ea8605c6c842a7
VitorLima1998 <vitor.xlr8@gmail.com>
2023-07-25 20:30:35 -0400

    fix: payment to fixed

--- src/app/api/payment/success/route.ts
+++ src/app/api/payment/success/route.ts
@@ -7,30 +7,40 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
 });
 
 export async function POST(request: Request) {
-  const sig = request.headers.get('stripe-signature')!;
+  try {
+    const sig = request.headers.get('stripe-signature')!;
 
-  const text = await request.text();
+    const text = await request.text();
 
-  const event = stripe.webhooks.constructEvent(
-    text,
-    sig,
-    process.env.STRIPE_WEBHOOK_SECRET_KEY!
-  );
+    const event = stripe.webhooks.constructEvent(
+      text,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET_KEY!
+    );
 
-  if (event.type === 'checkout.session.completed') {
-    const session = event.data.object as any;
+    if (event.type === 'checkout.session.completed') {
+      const session = event.data.object as any;
 
-    await prisma.tripReservation.create({
-      data: {
-        startDate: new Date(session.metadata.startDate),
-        endDate: new Date(session.metadata.endDate),
-        userId: session.metadata.userId,
-        tripId: session.metadata.tripId,
-        totalPaid: Number(session.metadata.totalPrice),
-        guests: Number(session.metadata.guests),
-      },
+      await prisma.tripReservation.create({
+        data: {
+          startDate: new Date(session.metadata.startDate),
+          endDate: new Date(session.metadata.endDate),
+          userId: session.metadata.userId,
+          tripId: session.metadata.tripId,
+          totalPaid: Number(session.metadata.totalPrice),
+          guests: Number(session.metadata.guests),
+        },
+      });
+    }
+
+    return new NextResponse(JSON.stringify({ received: true }), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log('error payment', error);
+
+    return new NextResponse(JSON.stringify({ success: false, error: error }), {
+      status: 400,
     });
   }
-
-  return new NextResponse(JSON.stringify({ received: true }), { status: 200 });
 }
